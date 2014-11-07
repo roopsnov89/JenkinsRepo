b0faea88fa7f195eb6ebdee96bfebb2714130809
Roopam <roopsnov89@gmail.com>
2014-11-07 11:50:30 +0530

    jenkins update

---
+++ src/test/java/com/company/testclasses/GoogleTest.java
@@ -0,0 +1,13 @@
+package com.company.testclasses;
+
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+	
+	@Test
+	public void New(){
+		System.out.println("hello world");
+	}
+
+
+}
