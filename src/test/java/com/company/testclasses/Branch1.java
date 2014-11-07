cbfd629218fce8f125f2d54c5d6667f079a81e70
Roopam <roopsnov89@gmail.com>
2014-11-07 12:29:59 +0530

    new

---
+++ src/test/java/com/company/testclasses/Branch1.java
@@ -0,0 +1,11 @@
+package com.company.testclasses;
+
+import org.testng.annotations.Test;
+
+public class Branch1 {
+	@Test
+	public void Branch(){
+		System.out.println("Hello this is branch1");
+	}
+
+}
